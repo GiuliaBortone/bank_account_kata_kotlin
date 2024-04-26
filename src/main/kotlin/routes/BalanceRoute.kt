46da5ce601e84605c9fc22641ed11d016f15068b
GiuliaBortone <giulia.bortone@mail.polimi.it>
2024-04-26 18:05:05 +0200

    Wire GetBalanceUsecase with BalanceRoute

--- src/main/kotlin/routes/BalanceRoute.kt
+++ src/main/kotlin/routes/BalanceRoute.kt
@@ -3,24 +3,28 @@ package routes
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import repositories.BankRepository
+import usecase.GetBalanceUsecase
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 class BalanceRoute: Route() {
     override fun handleGet(req: HttpServletRequest, resp: HttpServletResponse, bankRepository: BankRepository) {
-        val accountUUID = accountUUIDIfAccountExists(req, resp, bankRepository)
-
-        if (accountUUID == null) return
-
-        resp.status = HttpServletResponse.SC_OK
-        val formattedDate = ZonedDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-        val accountBalance = bankRepository.balanceFor(accountUUID)
-
-        resp.writer.print(
-            """{
+        try {
+            val usecase = GetBalanceUsecase(bankRepository) // inject me ?
+            val accountUUID = extractUUID(req)
+            val balance = usecase.execute(accountUUID)
+            val formattedDate = balance.dateTime.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+            resp.status = HttpServletResponse.SC_OK
+            resp.writer.print(
+                """{
                     "date": "$formattedDate",
-                    "balance": $accountBalance
+                    "balance": ${balance.amount}
                 }"""
-        )
+            )
+        } catch (ex: GetBalanceUsecase.AccountNotFoundException) {
+            resp.status = HttpServletResponse.SC_NOT_FOUND
+            return
+        }
+
     }
 }
